2e0233433d85c956a3d2e3004c85010010ac71e9
Frédéric Das-Neves <frederic@Frederics-MacBook-Pro.local>
2019-02-08 06:43:29 -0600

    Add messages for users

--- BoilerplateWebERP.API/Data/DataContext.cs
+++ BoilerplateWebERP.API/Data/DataContext.cs
@@ -15,6 +15,7 @@ namespace BoilerplateWebERP.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Like>()
@@ -30,6 +31,17 @@ namespace BoilerplateWebERP.API.Data
                 .HasOne(u => u.Liker)
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
